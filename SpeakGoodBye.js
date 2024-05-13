e74f485c5a926239f12aeaf2c3dbac4330131ea0
Bozhenadz <159878194+Bozhenadz@users.noreply.github.com>
2024-05-13 14:25:07 +0200

    SpeakGoodBye.js

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -1,1 +1,7 @@
-
+var speakWord = "Goodbye";
+export function speak(name) {
+  console.log(speakWord + " " + name);
+}
+export function speakAdd(name) {
+  console.log(speakWord + " Mr." + name);
+}
